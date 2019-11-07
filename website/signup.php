95aba95524e1b997d2b43908d942cdea174a490b
Lauro Vitor <laurovitor@gmail.com>
2019-11-07 00:24:18 -0300

    Update

--- website/signup.php
+++ website/signup.php
@@ -10,17 +10,16 @@ $cabecalho = array(
 
 $conteudo = json_encode(array('email' => $_POST['email'], 'name' => $_POST['name'], 'cpf' => $_POST['cpf'], 'password' => $_POST['password'], 'passwordConfirmation' => $_POST['password']));
 
-$url = 'http://' . $_ENV['API_URL'] . ':' . $_ENV['API_PORT'] . '/customer/customer';
+$url = 'http://' . $_ENV['API_URL'] . ':' . $_ENV['API_PORT'] . '/customer';
 
 $respostaApi = $api->enviaConteudoParaAPI($cabecalho, $conteudo, $url, 'POST');
 
 $resposta = json_decode($respostaApi, true);
 
-var_dump($resposta);
-/*
+//var_dump($resposta);
+
 if ($resposta["error"]) {
     $_SESSION["error"] = $resposta["error"];
     header('Location: register.php');
     exit;
 }
-*/
