05ea86cec0c443773388952da7c2c821057377be
Lauro Vitor <laurovitor@gmail.com>
2019-11-09 14:23:53 -0300

    Atuaslizações na Api e Controllers

--- website/classes/api.class.php
+++ website/classes/api.class.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'communication.class.php';
+include 'customer.controller.class.php';
 
 class API
 {
@@ -9,72 +9,26 @@ class API
         'Accept: json',
         'Authorization: Bearer ' . $_SESSION["token"]
     );
-    private static $communication = new Communication;
 
     public static function authenticate($email, $password)
     {
-        $content = json_encode(array('email' => $email, 'password' => $password));
-        $url = '/customer/authenticate';
-        $answer = self::$communication->SendContentToAPI(self::$header, $content, $url, 'POST');
-
-        $answer = json_decode($answer, true);
-        if ($answer["error"])
-            $_SESSION["error"] = $answer["error"];
-
-        if ($answer["customer"])
-            $_SESSION["customer"] = $answer["customer"];
-
-        if ($answer["token"]) {
-            $_SESSION["token"] = $answer["token"];
+        if (customerController::authenticate(self::$header, $email, $password))
             return "dashboard";
-        }
 
         return null;
     }
 
     public static function register($email, $name, $cpf, $password, $passwordConfirmation)
     {
-        $content = json_encode(array('email' => $email, 'name' => $name, 'cpf' => $cpf, 'password' => $password, 'passwordConfirmation' => $passwordConfirmation));
-        $url = '/customer';
-        $answer = self::$communication->SendContentToAPI(self::$header, $content, $url, 'POST');
-
-        $answer = json_decode($answer, true);
-
-        if ($answer["error"]) {
-            $_SESSION["error"] = $answer["error"];
-            return "register";
-        }
-
-        if ($answer["token"]) {
-            if ($answer["customer"])
-                $_SESSION["customer"] = $answer["customer"];
-
-            $_SESSION["token"] = $answer["token"];
+        if (customerController::add(self::$header, $email, $name, $cpf, $password, $passwordConfirmation))
             return "dashboard";
-        }
 
-        return null;
+        return "register";
     }
 
     public static function updateCustomer($email, $name, $cpf)
     {
-        $content = array_diff(array('email' => $email, 'name' => $name, 'cpf' => $cpf), $_SESSION['customer']);
-        $content = json_encode($content);
-
-        $url = '/customer/' . $_SESSION['customer']['_id'];
-
-        $answer = self::$communication->SendContentToAPI(self::$header, $content, $url, 'PATCH');
-
-        $answer = json_decode($answer, true);
-
-        if ($answer["error"])
-            $_SESSION["error"] = $answer["error"];
-
-        if ($answer["success"])
-            $_SESSION["success"] = $answer["success"];
-
-        if ($answer["customer"])
-            $_SESSION["customer"] = $answer["customer"];
+        customerController::update(self::$header, array($email, $name, $cpf));
 
         return "dashboard";
     }
--- website/classes/customer.controller.class.php
+++ website/classes/customer.controller.class.php
@@ -4,8 +4,26 @@ include 'communication.class.php';
 
 class customerController
 {
-    public static function authenticate($email, $password)
+    private static $communication = new Communication;
+
+    public static function authenticate($header, $email, $password)
     {
+        $content = json_encode(array('email' => $email, 'password' => $password));
+        $url = '/customer/authenticate';
+        $answer = self::$communication->SendContentToAPI($header, $content, $url, 'POST');
+
+        $answer = json_decode($answer, true);
+        if ($answer["error"])
+            $_SESSION["error"] = $answer["error"];
+
+        if ($answer["customer"])
+            $_SESSION["customer"] = $answer["customer"];
+
+        if ($answer["token"]) {
+            $_SESSION["token"] = $answer["token"];
+            return true;
+        }
+
         return false;
     }
 
@@ -24,13 +42,46 @@ class customerController
         return array();
     }
 
-    public static function add($email, $name, $cpf, $password, $passwordConfirmation)
+    public static function add($header, $email, $name, $cpf, $password, $passwordConfirmation)
     {
+        $content = json_encode(array('email' => $email, 'name' => $name, 'cpf' => $cpf, 'password' => $password, 'passwordConfirmation' => $passwordConfirmation));
+        $url = '/customer';
+        $answer = self::$communication->SendContentToAPI($header, $content, $url, 'POST');
+
+        $answer = json_decode($answer, true);
+
+        if ($answer["error"])
+            $_SESSION["error"] = $answer["error"];
+
+        if ($answer["token"]) {
+            $_SESSION["token"] = $answer["token"];
+            $_SESSION["customer"] = $answer["customer"];
+            return true;
+        }
+
         return false;
     }
 
-    public static function update($id)
+    public static function update($header, $customerArray)
     {
+        $content = array_diff($customerArray, $_SESSION['customer']);
+        $content = json_encode($content);
+
+        $url = '/customer/' . $_SESSION['customer']['_id'];
+
+        $answer = self::$communication->SendContentToAPI($header, $content, $url, 'PATCH');
+
+        $answer = json_decode($answer, true);
+
+        if ($answer["error"])
+            $_SESSION["error"] = $answer["error"];
+
+        if ($answer["success"]) {
+            $_SESSION["success"] = $answer["success"];
+            $_SESSION["customer"] = $answer["customer"];
+            return true;
+        }
+
         return false;
     }
 }
